46f031a28adcd84747cba5f70e32a83da1c145da
SkyzohKey <skyzohkey@protonmail.com>
2016-04-14 10:27:40 +0200

    chore(travis): add author mail address * Zetok asked me to add the mail address, that's done.

--- tools/verify_commit_message.sh
+++ tools/verify_commit_message.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#    Copyright © 2016 Zetok Zalbavar
+#    Copyright © 2016 Zetok Zalbavar <zetok@openmailbox.org>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
