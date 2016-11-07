d313fba29ed99565e801c2891f96d5d43997b1cb
SkyzohKey <skyzohkey@protonmail.com>
2016-11-07 13:02:45 +0100

    Convert code to use Pelican static generator, will allow a more efficient way of handling translations and stuff.

---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'SkyzohKey <skyzohkey@framasphere.org>'
+SITENAME = u'Ricin'
+TAG_LINE = u'Dead simple, privacy oriented, instant messaging app!'
+
+PATH = 'content'
+TIMEZONE = 'UTC'
+DEFAULT_LANG = u'en'
+
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+DEFAULT_PAGINATION = False
+THEME = 'themes/default'
+
+# pelican is mainly aimed at blogs and a regular pelican template has only
+# a predefined set of files with predefined names which do blog things.
+# since we don't need blog things, we override that, making pelican process
+# all template files (that don't start with '_') and don't treat them as some
+# special blog templates.
+DIRECT_TEMPLATES = []
+TEMPLATE_PAGES = {}
+
+import fnmatch
+import os
+
+templates_dir = THEME + '/templates'
+for root, dirnames, filenames in os.walk(templates_dir):
+    for filename in fnmatch.filter(filenames, '*.html'):
+        if not filename.startswith("_"):
+            template = os.path.join(root, filename)[len(templates_dir)+1:]
+            TEMPLATE_PAGES[template] = template
+
